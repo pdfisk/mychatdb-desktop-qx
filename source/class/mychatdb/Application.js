7b6463fb79698691ea6c2dc323855751ab54e6e6
Peter Fisk <peter.fisk@gmail.com>
2025-08-11 12:19:40 -0400

    refactoring

--- source/class/mychatdb/Application.js
+++ source/class/mychatdb/Application.js
@@ -1,72 +1,22 @@
-/* ************************************************************************
-
-   Copyright: 2025 undefined
-
-   License: MIT license
-
-   Authors: undefined
-
-************************************************************************ */
-
-/**
- * This is the main application class of "mychatdb"
- *
- * @asset(mychatdb/*)
- */
 qx.Class.define("mychatdb.Application",
-{
-  extend : qx.application.Standalone,
-
-
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-
-  members :
   {
-    /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
-     * 
-     * @lint ignoreDeprecated(alert)
-     */
-    main : function()
-    {
-      // Call super class
-      this.base(arguments);
+    extend: qx.application.Standalone,
 
-      // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
+    members:
+    {
+
+      main: function () {
+        this.base(arguments);
+
+        var button1 = new qx.ui.form.Button("Yo AGAIN DUDE!", "mychatdb/test.png");
+
+        var doc = this.getRoot();
+        doc.add(button1, { left: 100, top: 50 });
+
+        button1.addListener("execute", function () {
+          alert("Yo THERE AGAIN DUDE!!!");
+        });
       }
 
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
-
-      // Create a button
-      var button1 = new qx.ui.form.Button("Yo!!!", "mychatdb/test.png");
-
-      // Document is the application root
-      var doc = this.getRoot();
-
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
-
-      // Add an event listener
-      button1.addListener("execute", function() {
-        /* eslint no-alert: "off" */
-        alert("Yo DUDE!!!");
-      });
     }
-  }
-});
+  });
