226fd91440799702a89d7f00c17c9a70c50076e5
Peter Fisk <peter.fisk@gmail.com>
2025-08-23 13:48:17 -0400

    version

--- source/class/mychatdb/constants/Version.js
+++ source/class/mychatdb/constants/Version.js
@@ -2,8 +2,8 @@ qx.Class.define('mychatdb.constants.Version', {
     type: 'static',
 
     statics: {
-        timestamp: '2025-08-22T14:25:25Z',
-        version: '1.0.28',
+        timestamp: '2025-08-23T16:59:10Z',
+        version: '1.0.21',
     },
 
 });
