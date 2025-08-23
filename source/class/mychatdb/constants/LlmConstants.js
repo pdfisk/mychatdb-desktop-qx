4f1e9214c18909b8c9416cb2897b0dba8873797f
Peter Fisk <peter.fisk@gmail.com>
2025-08-23 12:22:09 -0400

    key

--- source/class/mychatdb/constants/LlmConstants.js
+++ source/class/mychatdb/constants/LlmConstants.js
@@ -2,9 +2,16 @@ qx.Class.define("mychatdb.constants.LlmConstants", {
     type: 'static',
 
     statics: {
-        GEMINI_API_ENDPOINT: 'https://generativelanguage.googleapis.com',
+        GEMINI_API_ENDPOINT: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
         GEMINI_API_KEY: 'AIzaSyCxyH23ynapHtEASHfD4IrPFJmWSNEjK_M',
-        LMS_API_ENDPOINT: 'http://localhost:1234'
+        GEMINI_MODEL: 'gemini-2.5-flash-lite',
+
+        LMS_API_ENDPOINT: 'http://localhost:1234',
+        LMS_MODEL: 'local-model',
+
+        OPENAI_API_ENDPOINT: 'https://api.openai.com',
+        XXXX: 'XXXXX:::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',
+        OPENAI_MODEL: 'gpt-4o'
     }
 
 });
